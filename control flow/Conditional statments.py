6bb6152d5edbe20852ebf556b227c4248066cc81
Faizan Ali <156774678+Faizan-207@users.noreply.github.com>
2024-03-12 22:38:30 +0500

    Conditional statments

---
+++ control flow/Conditional statments.py
@@ -0,0 +1,12 @@
+# if else statment
+""" age = int(input("Your age :"))
+if age > 18:
+    print("Congratulation your are eligible")
+else:
+    print("Not eligible")
+
+    """
+# in one line
+age = int(input("Enter your age:"))
+message ="Eligible" if age > 18 else "Not eligible"
+print(message)
