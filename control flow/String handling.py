77fa4b525f77a2eccd16e13960f4119c2f56b911
Faizan Ali <156774678+Faizan-207@users.noreply.github.com>
2024-03-12 22:04:19 +0500

    String handling

---
+++ control flow/String handling.py
@@ -0,0 +1,15 @@
+name = "Faizan Ali"
+father = "Akmal waheed"
+print(name.upper())
+print( "Ali" in name)
+print(father.find("w"))
+print(name.lower())
+print(father.title())
+print(name.format())
+print(father.strip()) #String fnctions
+
+print(name[:6]) #Acessing a specifc element
+print(father[6:])
+full = name +" "+ father #Srting concatenation
+print(full)
+print(f"{name} {father}") #Alternate of concatination , string formating 
