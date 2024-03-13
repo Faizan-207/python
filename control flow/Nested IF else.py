3de384fc0a549f6b770b34008eb30fc8176e2fa1
Faizan Ali <156774678+Faizan-207@users.noreply.github.com>
2024-03-13 05:40:46 +0500

    Delete control flow/Nested IF else.py

--- control flow/Nested IF else.py
+++
@@ -1,8 +0,0 @@
-salary = int(input("Enter your salray"))
-if salary >= 20000:
-    net = salary - (salary * 0.07) # Deduct 7% of  orignal salary
-elif salary >=10000:
-    net = salary - (salary * 0.02)# Deduct 2% of orignal salray
-else:
-    net = salary
-print("Your net salary is :",net)
