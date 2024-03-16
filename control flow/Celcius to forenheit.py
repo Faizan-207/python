31c2b6d5e1b6d241926516f11a203991d68827b5
Faizan Ali <156774678+Faizan-207@users.noreply.github.com>
2024-03-16 11:31:00 +0500

    `C ----> `F

---
+++ control flow/Celcius to forenheit.py
@@ -0,0 +1,6 @@
+celsius = float(input("Enter temperature in Celsius:"))
+#converting Celcius -----> Forenheit
+forenheit = (celsius * 9 / 5) +32
+
+#printing tempature in forenheit
+print ("Tempature in forenheit",forenheit)
