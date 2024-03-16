368ff63415ca7d12299a1e7a01f4744a632f9256
Faizan Ali <156774678+Faizan-207@users.noreply.github.com>
2024-03-16 11:30:04 +0500

    Type_casting

---
+++ control flow/Typecasting.py
@@ -0,0 +1,16 @@
+a = 3
+b = 6
+c = 9
+d = 3.9
+e = 5
+ 
+a_str = str(a)
+b_str = str(b)
+c_str = str(c)
+final_str = str(a) + str(b) + str(c)
+final_int = int (final_str)
+print("Final_int =",final_int,type(final_str))
+
+int_d = int(d)
+float_e = float(e)
+print(int_d,float_e,sep="<>")
